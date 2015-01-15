eb3d2e29557df62efbb4b92165c00695d6a769be
niq414 <laniqua.webb@aim.com>
2015-01-14 20:57:31 -0500

    Arrays
    Muppet Names

--- assignment (Values and Variables)/JS/script.js
+++ assignment (Values and Variables)/JS/script.js
@@ -1,3 +1,6 @@
 // Arrays
 
-var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"];
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+
+console.log(muppetNames[1]);//lets me see whats in the index #1
+muppetNames[1] = "Rizzo";
