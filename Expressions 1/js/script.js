4a634c5e3a012a74f4ad67d93108465600078876
niq414 <laniqua.webb@aim.com>
2015-01-14 23:18:22 -0500

    Modulo

--- Expressions 1/js/script.js
+++ Expressions 1/js/script.js
@@ -1,10 +1,7 @@
 //Expressions
 
-//area of the triangle is half of the width times the height
-var width = 8;
-var height = 7;
-var area = width * height/2;
-console.log(area);
+var remainder = 211 % 2;
+console.log(remainder);
 
 
 
