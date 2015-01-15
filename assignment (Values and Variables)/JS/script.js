f552e77a0ec69b4ee8a58f503b6080b735bff24d
niq414 <laniqua.webb@aim.com>
2015-01-14 21:35:51 -0500

    Alerts

--- assignment (Values and Variables)/JS/script.js
+++ assignment (Values and Variables)/JS/script.js
@@ -1,4 +1,9 @@
 //Prompt and Alert
 
-var userInput = prompt("Enter your year of birth");
-console.log(userInput);
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height");
+var area =  width * height; //calculates with that info
+//console.log(area); //prints it out to console
+
+var result = ("The area of your rectangle is " + area)
+alert(result);
