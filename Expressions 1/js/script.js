5b5bc6415acdae3cf9a0a3590f6068e80f897a71
niq414 <laniqua.webb@aim.com>
2015-01-14 23:08:56 -0500

    Age

--- Expressions 1/js/script.js
+++ Expressions 1/js/script.js
@@ -1,10 +1,8 @@
 //Expressions
 
-var a = 2; //sets up or variable a and defines it with 2
-var b;
-b = a + 3; // add 3 to a
-console.log(a);
-
+var yearBorn = 1985;
+var age = 2015 - yearBorn - 1;
+console.log(age);
 
 
 
