751c5f4ce332c0facad0ce0ba052e862b1215fb2
niq414 <laniqua.webb@aim.com>
2015-01-15 19:49:39 -0500

    Complete Phone Number

--- Expressions 1/js/script.js
+++ Expressions 1/js/script.js
@@ -4,7 +4,7 @@ var areaCode = 407;
 var firstPart = 203;
 var secPart = 4529;
 
-var phoneNo = String(areaCode) + String(firstPart) + String(secPart);
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
 console.log(phoneNo);
 
 
