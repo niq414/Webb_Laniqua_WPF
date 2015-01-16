2bab37193f16a117cf6d644d4c5c5eea2d86b0ff
niq414 <laniqua.webb@aim.com>
2015-01-15 19:26:00 -0500

    Concatenating Strings

--- Expressions 1/js/script.js
+++ Expressions 1/js/script.js
@@ -1,8 +1,11 @@
 //Expressions
 
-var total = orangeBins = [234, 567,883];
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
-console.log(total);
+var firstName = "Kermit";
+var lastName = "The Frog";
+var fullName = firstName + lastName;
+
+console.log(fullName);
+
 
 
 
