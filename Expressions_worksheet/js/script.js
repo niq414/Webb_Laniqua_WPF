681c6fca5e1c1e4bdb83900f6f5d00da7aa4c1fe
niq414 <laniqua.webb@aim.com>
2015-01-15 23:38:59 -0500

    Created Dog Years calculation
    Created a prompt that calculates how old a dog is in dog years based.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,25 @@
+//Laniqua Webb January 14, 2015 Expression Worksheet
+
+
+
+//Calculating how old Sparky the pit bull is in dog years
+
+//Defining the variables
+var humanYears = a = 1;
+var dogYears = b = 7;
+
+//Ask the user how old Sparky is in human years
+var humanYear = prompt("What is Sparky\'s age in human years?");
+console.log(humanYear);
+
+
+//multiply humanYears and dogYears to determine the how many dog years Sparky is
+var dogYear = humanYear * 7;
+
+//Print out the number of years Sparky is
+console.log("Sparky is " + dogYear);
+alert ("Sparky is " + dogYear + " in dog years!");
+
+//Sparky is 5 human years old which is 35 in dog years
+
+
