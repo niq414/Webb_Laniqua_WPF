06e739cb5178c89a4de76f526664a1b2c9a94f5d
niq414 <laniqua.webb@aim.com>
2015-01-15 19:51:20 -0500

    Assignment Operators

--- Expressions 1/js/script.js
+++ Expressions 1/js/script.js
@@ -1,11 +1,8 @@
 //Expressions
 
-var areaCode = 407;
-var firstPart = 203;
-var secPart = 4529;
-
-var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
-console.log(phoneNo);
+var a = 3;
+a + 4;
+console.log(a);
 
 
 
