12c7353a4545cfdb9a011569593372cf7c974caa
niq414 <laniqua.webb@aim.com>
2015-01-18 20:08:54 -0500

    Expressions
    Created final output

--- Webb_Laniqua_Expressions_Assignment/js/script.js
+++ Webb_Laniqua_Expressions_Assignment/js/script.js
@@ -19,5 +19,6 @@ var hairPacks = boxBraid * 5 + goddessBraids * 3 + upDo * 2;
 
 //Alert the user of the total amount of hair packs needed based on the hairstyle for each client
 
+console.log("You will need " + hairPacks + " packs of hair.")
 
 
