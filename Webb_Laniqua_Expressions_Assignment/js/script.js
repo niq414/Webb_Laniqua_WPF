873e81790ee4bfa3a9da1f98b758279e385df7a7
niq414 <laniqua.webb@aim.com>
2015-01-18 20:05:54 -0500

    Expressions Assignment
    Created values and provided foundational information

---
+++ Webb_Laniqua_Expressions_Assignment/js/script.js
@@ -0,0 +1,10 @@
+//Laniqua Webb January 15, 2015 Expressions Assignment
+
+//This calculator will calculate how many packs of hair a hairstylist will need for her scheduled clients
+
+// The amount of hair packs that each style requires
+var boxBraid = a = 5;
+var goddessBraids = b = 3;
+var upDo = c = 2;
+
+
