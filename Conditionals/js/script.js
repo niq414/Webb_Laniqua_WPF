92ed9864c339af05c5fba89d29c28b7dfae912f3
niq414 <laniqua.webb@aim.com>
2015-01-21 22:23:42 -0500

    Kid Height

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,12 +1,15 @@
 //Conditional Logic - with an expression
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift = 2;
+
 //if the child is old enough, print to the console "you can ride"
 //if the kid is over 48 inches in height
 
-if(kidHeight + 2 > minHeight) {
+if(kidHeight > minHeight) {
 //code performed if condition is true
     console.log("You can ride the coaster!");
+}
+if(kidHeight <= minHeight){
+    console.log("Sorry kid, You've got some growing to do first!")
 }
