1ea258d24e63aabecbaaf399ba97e616835c8ef6
niq414 <laniqua.webb@aim.com>
2015-01-21 22:56:14 -0500

    Iphone

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,15 +1,17 @@
-//Conditional Logic - with an expression
+//Conditional Logic  - Logical Operators
 
-var kidHeight = 52;
-var minHeight = 48;
+var budget = 300;
+var iPhonePrice = 199.99;
+var payCheck = 200;
 
-//if the child is old enough, print to the console "you can ride"
-//if the kid is over 48 inches in height
-
-if(kidHeight > minHeight) {
-//code performed if condition is true
-    console.log("You can ride the coaster!");
+//if the price of the phone is less than our budget And if our paycheck is over 300
+if(iPhonePrice < budget){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!!")
 }
-if(kidHeight <= minHeight){
-    console.log("Sorry kid, You've got some growing to do first!")
+if (payCheck > 300){
+    console.log("We can buy the phone!")
+}else{
+    console.log("No phone for you!!")
 }
