4fbcb2d07b252a019e6685c038a13bcf2eb9f184
niq414 <laniqua.webb@aim.com>
2015-01-21 23:18:31 -0500

    Ternary

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,9 +1,11 @@
 //Conditional Logic  - Ternary Operators
 
-var gpa = 48;
+var age = 4
+var book;
 
-//if the gpa is over the min 2.0 score, the student can graduate
-/*if(gpa > 2.0){
-    console.log("You can graduate!")
-}*/
-(gpa > 2.0) ? console.log("You can graduate!")
+//if the child is under 10, they get Green Eggs and Ham. otherwise they get The Time Machine
+
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
+
+
