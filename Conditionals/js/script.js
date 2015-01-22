943d000e48ebcc67585b135f80a52b2dac9743a6
niq414 <laniqua.webb@aim.com>
2015-01-21 22:20:10 -0500

    Sneaker Lift

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,12 +1,12 @@
-//Conditional Logic
+//Conditional Logic - with an expression
 
-var kidHeight = 40;
+var kidHeight = 47;
 var minHeight = 48;
-
+var sneakerLift = 2;
 //if the child is old enough, print to the console "you can ride"
 //if the kid is over 48 inches in height
 
-if(kidHeight > minHeight) {
+if(kidHeight + 2 > minHeight) {
 //code performed if condition is true
     console.log("You can ride the coaster!");
 }
