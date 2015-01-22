bebdaa3f3e0fce5d3fc94e7ef21db9afb46cb164
niq414 <laniqua.webb@aim.com>
2015-01-21 23:07:27 -0500

    Won Lottery

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,17 +1,12 @@
 //Conditional Logic  - Logical Operators
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var payCheck = 200;
+var wonLottery = true;
 
 //if the price of the phone is less than our budget And if our paycheck is over 300
-if(iPhonePrice < budget){
+if(iPhonePrice < budget || wonLottery){
     console.log("We can buy the phone!");
-}else{
-    console.log("No phone for you!!")
-}
-if (payCheck > 300){
-    console.log("We can buy the phone!")
 }else{
     console.log("No phone for you!!")
 }
